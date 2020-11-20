c1f6e55151a24bc2259fef661ca35fdbbe2411fd
Breein <grig.cem@gmail.com>
2020-11-20 22:59:00 +0700

    Верстка чата и размеров интерфейса, работает но средне.

--- src/cookie-roulette/GameTable/Chat.js
+++ src/cookie-roulette/GameTable/Chat.js
@@ -5,19 +5,33 @@ import React from 'react';
 class Chat extends React.Component{
   constructor(props) {
     super(props);
+
+    this._n = 'colored-3';
   }
 
   render() {
     return (
       <article className="chat">
         <header>
-          <div>Общий чат</div>
-          <div>Личные сообщения</div>
+          <div className="selected">Общий чат</div>
+          <div>
+            <span>Личные сообщения <em>10</em></span>
+          </div>
         </header>
-        <section className="messages" />
+        <section className="messages">
+          <article className="message my-message"><legend>Вы говорите: </legend>Привет мир!</article>
+          <article className="message"><legend><span className={this._n}>Василий</span> говорит: </legend>Привет мир!</article>
+          <article className="message my-message"><legend>Вы говорите: </legend>Привет мир!</article>
+          <article className="message my-message"><legend>Вы говорите: </legend>фроыво оыырвфры оымвоывр мы нып овиоы пы впыом нвп офыавымпфо авфым пафывр мрыфм </article>
+          <article className="message"><legend><span className={this._n}>Василий</span> говорит: </legend>Привет мир!</article>
+          <article className="message my-message"><legend>Вы говорите: </legend>Привет мир!</article>
+          <article className="message my-message"><legend>Вы говорите: </legend>Привет мир!</article>
+          <article className="message"><legend><span className={this._n}>Василий</span> говорит: </legend>Привет мир!</article>
+          <article className="message my-message"><legend>Вы говорите: </legend>Привет мир!</article>
+        </section>
         <footer>
-          <input type="text" value="Написать в чат"/>
-          <input type="button"/>
+          <input type="text" value="" placeholder="Написать в чат"/>
+          <input type="button" value="►"/>
         </footer>
       </article>
     );
