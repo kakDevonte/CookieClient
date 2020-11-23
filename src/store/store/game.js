c114459eb2b7b72a35cbbed5c704e3fc93234b85
Breein <grig.cem@gmail.com>
2020-11-24 00:40:56 +0700

    Новый стор и собственно теперь вся логика там. (вероятнее всего потом перенесу). + куча логов (убрать позже) храним количество людей по группам. изменение логики работы, теперь игроки обновляются только нужные, дабы не рендерить все повторно. + метод начала игры, повзоляющий начать крутить печеньку при соблюдении условий. + метод крути, отправляющий на сервер данные.

--- src/store/store/game.js
+++ src/store/store/game.js
@@ -1,15 +1,23 @@
 import {makeObservable, observable, action} from "mobx";
 import io from "socket.io-client";
 import common from "../../config/common";
+import Collection from "../../helpers/Collection";
 
 class GameStore {
 
   _tid = null;
   _stage = 'connection';
-  _players = [];
-  _currentTurn = 0;
+  _players = new Map();
+  _currentTurn = null;
+  _groups = {
+    male: 0,
+    female: 0
+  };
 
   constructor (store) {
+    this.getPlayer = this.getPlayer.bind(this);
+    this._players.set('getPlayer', this.getPlayer);
+
     makeObservable(this, {
       _tid: observable,
       _stage: observable,
@@ -18,7 +26,9 @@ class GameStore {
 
       setTID: action,
       setStage: action,
-      setPlayers: action
+      setPlayer: action,
+      updatePlayers: action,
+      setCurrentTurn: action
     });
 
     this._store = store;
@@ -28,35 +38,31 @@ class GameStore {
     this._sockets();
   }
 
-  _connect(){
+  _connect() {
     this._socket = io(process.env.REACT_APP_SOCKET_SERVER);
   }
 
-  // get game(){
-  //   return this.game;
-  // }
-
-  get tid(){
+  get tid() {
     return this._tid;
   }
 
-  get stage(){
+  get stage() {
     return this._stage;
   }
 
-  get players(){
+  get players() {
     return this._players;
   }
 
-  player(index){
-    return this._players[index];
+  getPlayer(index) {
+    return this._players.get(index);
   }
 
-  get currentTurn(){
+  get currentTurn() {
     return this._currentTurn;
   }
 
-  setTID(tid){
+  setTID(tid) {
     this._tid = tid;
   }
 
@@ -64,24 +70,29 @@ class GameStore {
     this._stage = stage;
   }
 
-  setPlayers(players) {
-    this._players = players;
+  setPlayer(index, object) {
+    this._players.set(index, object);
   }
 
+  setCurrentTurn(index){
+    this._currentTurn = index;
+  }
 
-  _sockets(){
+  _sockets() {
     const
       store = this._store,
       socket = this._socket;
 
     socket.on('request-info', () => {
       socket.emit('user-info', store.user.data);
+      console.log('request-info');
     });
 
     socket.on('connect-success', () => {
       if(this.stage !== 'lobby'){
         socket.emit('in-lobby');
         this.setStage('lobby');
+        console.log('connect-success');
       }
     });
 
@@ -92,30 +103,88 @@ class GameStore {
       this.setTID(response.tid);
       this.setStage('table');
 
+      console.log('put-table');
       socket.emit('in-table', this.tid);
     });
 
     socket.on('update-players', (response) => {
-      const tid = response.tid;
-      const players = response.players;
+      this._groups = response.groups;
+      this.updatePlayers(response.tid, response.players);
+      this._startGame(response.current);
 
-      if(tid === this.tid) {
-        players.forEach(player => {
-          if(common.randomNumber(0, 1)) player.kissed.push(true);
-          if(player.id === store.user.data.id) {
-            player.itsMe = true;
-          }
-        });
-
-        this.setPlayers(players);
-      }
+      console.log("update-players", response.current);
     });
 
     socket.on('current-stage', (stage) => {
+      console.log('current-stage', stage);
       this.setStage(stage.current);
     });
 
+    socket.on('kiss-question', ({uid, seat}) => {
+      console.log(this.getPlayer(seat).name);
+    });
+
     socket.on('console', (res) => console.log(res));
+  }
+
+  _startGame(current) {
+    if(current === null) return;
+    if(this.currentTurn === current) return;
+
+    const player = Collection.takeOne(this._players, current + 1);
+
+    if(player === null) return;
+    if(player.id !== this._store.user.data.id) return;
+    if(this._isPlayersReady()) return;
+
+    this.setCurrentTurn(current);
+
+    setTimeout(() => {
+      this._rotateRoulette();
+    }, 1000);
+  }
+
+  _rotateRoulette(){
+    const data = {
+      tid: this.tid,
+      uid: this._store.user.data.id
+    };
+
+    console.log("Rotate roulette!");
+
+    this._socket.emit('rotate-roulette', data);
+  }
+
+  _isPlayersReady(){
+    return (this._groups.male < 2 && this._groups.female < 2);
+  }
+
+
+  updatePlayers(tid, players){
+    if(tid === this.tid) {
+      players.forEach((player, index) => {
+        let update;
+        const current = this.getPlayer(index);
+
+        if(player){
+          if(player.id === this._store.user.data.id) {
+            player.itsMe = true;
+          }
+
+          if(!current) {
+            update = true;
+          }
+
+          if(current && current.id !== player.id) {
+            update = true;
+          }
+        } else {
+          if(current) update = true;
+        }
+
+        if(update) this._players.set(index, player);
+      });
+    }
   }
 }
 
