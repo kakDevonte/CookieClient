9caf64d4a0ba46675df362f47f1d2c0b3cad7dc8
Breein <grig.cem@gmail.com>
2020-11-24 00:34:37 +0700

    Новый механгизм работы. Берем игровок из mobx

--- src/cookie-roulette/GameTable/GameTable.js
+++ src/cookie-roulette/GameTable/GameTable.js
@@ -9,26 +9,32 @@ import Chat from "./Chat";
 
 import { inject, observer } from "mobx-react";
 
-const GameTable = ({store, game}) => {
+const GameTable = ({store}) => {
+
+  //const player = players.get('getPlayer');
+
+  const player = store.game.getPlayer;
+
+  //console.log('Render Table!');
 
   return (
       <section className="game">
         <article className="table">
           <HeaderMenu />
           <div className="wrap-players">
-            <Player user={game.player(7)} index="7" />
-            <Player user={game.player(0)} index="0" />
-            <Player user={game.player(1)} index="1" />
+            <Player player={player(7)} index={7} />
+            <Player player={player(0)} index={0} />
+            <Player player={player(1)} index={1} />
           </div>
           <div className="wrap-players">
-            <Player user={game.player(6)} index="6" />
+            <Player player={player(6)} index={6} />
             <div className="cookie-space" />
-            <Player user={game.player(2)} index="2" />
+            <Player player={player(2)} index={2} />
           </div>
           <div className="wrap-players">
-            <Player user={game.player(5)} index="5" />
-            <Player user={game.player(4)} index="4" />
-            <Player user={game.player(3)} index="3" />
+            <Player player={player(5)} index={5} />
+            <Player player={player(4)} index={4} />
+            <Player player={player(3)} index={3} />
           </div>
           <span className='table-id'>{store.game.tid}</span>
           <CookieSelector />
