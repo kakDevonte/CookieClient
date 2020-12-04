33170b83fe3e4329cf25007c358a7f8077656d36
Breein <grig.cem@gmail.com>
2020-12-04 13:04:45 +0700

    Компонент ожидания игры

---
+++ src/cookie-roulette/GameTable/WaitMorePlayers.js
@@ -0,0 +1,20 @@
+import '../../css/wait-players.css';
+
+import React from 'react';
+import {inject, observer} from "mobx-react";
+
+function WaitMorePlayers({store}) {
+  const className = () => {
+    if(store.game.state === 'pending') return 'wait-players show';
+    return 'wait-players';
+  };
+
+  return (
+    <div className={className()}>
+      <header>Недостаточно<br/>игроков</header>
+      <span>Ожидаем наполнения<br/>стола</span>
+    </div>
+  )
+}
+
+export default inject('store')( observer(WaitMorePlayers) );
