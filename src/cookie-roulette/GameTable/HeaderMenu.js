2c2abecb5debd7d997e2f0601cdf9eded4b9095b
Breein <grig.cem@gmail.com>
2020-12-04 17:22:23 +0700

    Функциональный компонент. И вывод общих поцелуев.

--- src/cookie-roulette/GameTable/HeaderMenu.js
+++ src/cookie-roulette/GameTable/HeaderMenu.js
@@ -1,31 +1,32 @@
 import '../../css/header-menu.css';
 
 import React from 'react';
+import {inject, observer} from "mobx-react";
 
-class HeaderMenu extends React.Component{
-  render() {
-    return (
-      <header className="header-menu">
-        <input type='button' className="exit-game" />
-        <section className="cookie-count">
-          <i />
-          <span className="center-screen">0</span>
-          <input type="button" />
-        </section>
-        <section className="kiss-count">
-          <i />
-          <span>0</span>
-        </section>
-        <input type='button' className="change-table" />
-        <input type='button' className="settings" />
-        <section className="vk-apps-overlay">
-          <input type='button' className="setup" />
-          <div />
-          <input type='button' className="exit-app" />
-        </section>
-      </header>
-    );
-  }
+function HeaderMenu({store}){
+
+  return (
+    <header className="header-menu">
+      <input type='button' className="exit-game" />
+      <section className="cookie-count">
+        <i />
+        <span className="center-screen">0</span>
+        <input type="button" />
+      </section>
+      <section className="kiss-count">
+        <i />
+        <span>{store.user.data.kissCounter}</span>
+      </section>
+      <input type='button' className="change-table" />
+      <input type='button' className="settings" />
+      <section className="vk-apps-overlay">
+        <input type='button' className="setup" />
+        <div />
+        <input type='button' className="exit-app" />
+      </section>
+    </header>
+  );
 }
 
-export default HeaderMenu;
+
+export default inject('store')( observer(HeaderMenu) );
