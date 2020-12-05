2ac7bd7c29004668181af688a81e067d17c479aa
Breein <grig.cem@gmail.com>
2020-12-05 16:03:51 +0700

    Доки и отправка стола при переход в лобби.

--- src/store/store/app.js
+++ src/store/store/app.js
@@ -22,13 +22,21 @@ class AppStore {
   setVersion(version) { this._version = version; }
   setStage(stage) { this._stage = stage; }
 
-  stageLobby() {
+  /**
+   * Переносит игрока в лобби
+   * @param {=string} tid - предыдущий стол
+   */
+  stageLobby(tid) {
     if(this.stage === 'lobby') return;
 
     this.setStage('lobby');
-    this._store.socket.emit('in-lobby');
+    this._store.socket.emit('in-lobby', tid);
   }
 
+  /**
+   * Переносит игрока на игровой стол
+   * @param {string} tid - текущий стол
+   */
   stageTable(tid) {
     if(this.stage === 'table') return;
 
