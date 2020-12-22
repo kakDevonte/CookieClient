cfa59ffc07b8c16a6d30a76846a588b1417d91e6
Breein <grig.cem@gmail.com>
2020-12-22 17:53:36 +0700

    Окно отоображения обрыва связи с сервером

---
+++ src/cookie-roulette/GameTable/Modal/ServerFail.js
@@ -0,0 +1,17 @@
+import './css/server-fail.css';
+
+import React from 'react';
+import {inject, observer} from "mobx-react";
+
+function ServerFail({store}){
+  const className = () => {
+    if(store.socket.serverFail) return 'server-fail center-screen opened';
+    return 'server-fail center-screen';
+  };
+
+  return (
+    <div className={ className() }>Игровой сервер не отвечает</div>
+  );
+}
+
+export default inject('store')( observer(ServerFail) );
