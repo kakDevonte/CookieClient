3cfd2a5fcc2554dd782b696f449af6be358ebafb
Breein <grig.cem@gmail.com>
2021-01-07 21:14:04 +0700

    Скрываем когда игра на приостановлена

--- src/cookie-roulette/GameTable/YourTurnTooltip.js
+++ src/cookie-roulette/GameTable/YourTurnTooltip.js
@@ -6,7 +6,7 @@ import {inject, observer} from "mobx-react";
 function YourTurnTooltip({store}) {
 
   const className = () => {
-    if(store.game.allowClickRotate) {
+    if(store.game.allowClickRotate && store.game.state !== 'pending') {
       return 'your-turn-tooltip enabled';
     } else {
       return 'your-turn-tooltip';
@@ -15,7 +15,7 @@ function YourTurnTooltip({store}) {
 
   return (
     <div className={ className() }>
-      Твой ход!<br/>Нажми на пченеьку,<br/>чтобы крутить!
+      Твой ход!<br/>Нажми на печеньку,<br/>чтобы крутить!
     </div>
   );
 }
